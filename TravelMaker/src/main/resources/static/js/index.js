cd04e9a98d20b3c17ded492b1c835d7080d3a119
hyeon605 <happilyah@naver.com>
2023-01-03 17:08:12 +0900

    2023-01-03 17:08 index 수정2(아현)

--- TravelMaker/src/main/resources/static/js/index.js
+++ TravelMaker/src/main/resources/static/js/index.js
@@ -2,6 +2,10 @@
  * 
  */
 
+$('.btnMyInfo').on('click',function(){
+	 $('#content').load('/myInfo/myInfo');
+ })
+
  function navigo (){
 	const hero = document.querySelector('#hero');	//hero부분 획득
 	const fixStart = hero.clientHeight;		//hero 높이
@@ -14,13 +18,41 @@
 	     header.style.position='fixed';
 	     header.style.backgroundColor='#697c89';
 	     header.style.opacity=0.55;
+	   }else {
+			if(document.getElementById("hero")){
+				header.style.position='absolute';
+				header.style.backgroundColor='rgb(255 255 255 / 0%)';
+				header.style.opacity=0; 			 
+			}else{
+				header.style.position='fixed';
+		     	header.style.backgroundColor='#697c89';
+		     	header.style.opacity=0.55;
+			}
 	   }
-	   else {
-	     header.style.position='absolute';
-	     header.style.backgroundColor='rgb(255 255 255 / 0%)';
-	     header.style.opacity=0;
-	     }
-	 } 
-  
+	 }
 }
 navigo()
+
+
+function div(){
+	const header = document.querySelector('header');
+	const empty = document.querySelector('#empty');
+	$('.btnMyInfo').on('click',function(){
+		empty.style.height='65px';
+		header.style.backgroundColor='#697c89';
+		header.style.opacity=0.55;
+	});
+ 	$('.btnSearch').on('click',function(){
+		empty.style.height='65px';
+		header.style.backgroundColor='#697c89';
+		header.style.opacity=0.55;
+ 	});
+ 	$('.reviewBoard').on('click',function(){
+		empty.style.height='65px';
+		header.style.backgroundColor='#697c89';
+		header.style.opacity=0.55;
+ 	});
+
+}
+div()
+
